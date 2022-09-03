83ff2bfbc504759e1122253539a5bb8c66d36f76
martintoledoo <68962843+martintoledoo@users.noreply.github.com>
2022-09-03 17:56:05 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,130 @@
+var form_1 = document.querySelector(".form_1");
+var form_2 = document.querySelector(".form_2");
+var form_3 = document.querySelector(".form_3");
+
+
+var form_1_btns = document.querySelector(".form_1_btns");
+var form_2_btns = document.querySelector(".form_2_btns");
+var form_3_btns = document.querySelector(".form_3_btns");
+
+
+var form_1_next_btn = document.querySelector(".form_1_btns .btn_next");
+var form_2_back_btn = document.querySelector(".form_2_btns .btn_back");
+var form_2_next_btn = document.querySelector(".form_2_btns .btn_next");
+var form_3_back_btn = document.querySelector(".form_3_btns .btn_back");
+
+var form_2_progessbar = document.querySelector(".form_2_progessbar");
+var form_3_progessbar = document.querySelector(".form_3_progessbar");
+
+var btn_done = document.querySelector(".btn_done");
+var modal_wrapper = document.querySelector(".modal_wrapper");
+var shadow = document.querySelector(".shadow");
+
+form_1_next_btn.addEventListener("click", function(){
+	if(validarFormularioSM("detallePedido")){
+		form_1.style.display = "none";
+		form_2.style.display = "block";
+	
+		form_1_btns.style.display = "none";
+		form_2_btns.style.display = "flex";
+	
+		form_2_progessbar.classList.add("active");
+	}
+});
+
+form_2_back_btn.addEventListener("click", function(){
+
+	form_1.style.display = "block";
+	form_2.style.display = "none";
+
+	form_1_btns.style.display = "flex";
+	form_2_btns.style.display = "none";
+
+	form_2_progessbar.classList.remove("active");
+});
+
+form_2_next_btn.addEventListener("click", function(){
+	if(validarFormularioSM("datosEntrega")){
+
+		form_2.style.display = "none";
+		form_3.style.display = "block";
+
+		form_3_btns.style.display = "flex";
+		form_2_btns.style.display = "none";
+
+		form_3_progessbar.classList.add("active");
+	}
+
+});
+
+form_3_back_btn.addEventListener("click", function(){
+
+		form_2.style.display = "block";
+		form_3.style.display = "none";
+
+		form_3_btns.style.display = "none";
+		form_2_btns.style.display = "flex";
+
+		form_3_progessbar.classList.remove("active");
+
+});
+
+btn_done.addEventListener("click", function(){
+	if(validarFormularioSM("formaPago")){
+		modal_wrapper.classList.add("active");
+	}
+
+})
+
+shadow.addEventListener("click", function(){
+	modal_wrapper.classList.remove("active");
+})
+
+
+function validarFormularioSM(formularioId){
+    var validado = true;
+    elementos = document.querySelectorAll(`#${formularioId} select, #${formularioId} input`);
+    for(i=0;i<elementos.length;i++){
+        if((elementos[i].value == "" || elementos[i].value == null) && elementos[i].type !='file' && elementos[i].disabled != true){
+            validado = false;
+            elementos[i].focus();
+            return false;
+        }
+    }
+    if(validado){
+        return true;
+    }
+
+}
+
+function selectValue(selectId){
+	let select = document.getElementById(selectId)
+	return select.options[select.selectedIndex].value;
+}
+
+function opcionEntrega(){
+	let fechaEntrega = document.getElementById("fechaEntrega")
+
+	let optEntrega = selectValue("entrega");
+
+	if (optEntrega == 2) {
+		fechaEntrega.disabled = false;
+	}else{
+		fechaEntrega.disabled = true;
+	}
+}
+
+function opcionPago(){
+	let pagoEfectivo = document.getElementById("pagoEfectivo")
+	let pagoTarjeta = document.getElementById("pagoTarjeta")
+
+	let optPago = selectValue("tipoPago");
+
+	if (optPago == 1) {
+		pagoEfectivo.hidden = false;
+		pagoTarjeta.hidden = true;
+	}else{
+		pagoEfectivo.hidden = true;
+		pagoTarjeta.hidden = false;
+	}
+}
